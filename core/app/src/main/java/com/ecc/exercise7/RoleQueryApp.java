9edacc38c098eba36a7c5b0f6d912ffe787c2f6c
Dominic Orga <dominicorga@gmail.com>
2018-11-22 13:09:20 +0800

    Add operation complete notification on apps

--- core/app/src/main/java/com/ecc/exercise7/RoleQueryApp.java
+++ core/app/src/main/java/com/ecc/exercise7/RoleQueryApp.java
@@ -37,15 +37,12 @@ public class RoleQueryApp {
 	    			break;
                 case ADD_ROLE :
                 	addRole();
-                	viewRoles();
                 	break;
                 case UPDATE_ROLE:
                 	updateRole();
-                	viewRoles();
                 	break;
                 case REMOVE_ROLE:
                 	removeRole();
-                	viewRoles();
                 	break;
                 case ASSIGN_ROLE:
                     assignRole();
@@ -55,6 +52,8 @@ public class RoleQueryApp {
                     break;
     			case RETURN:
     				isReturn = true;
+
+                System.out.println("Operation Complete.");
 	    	}	
 
     	} while (!isReturn);    	
